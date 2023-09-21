f9eb41f9c8ae66470441b1fc12aaa140b953a363
illocarlos <carlosalbendiz93@gmail.com>
2023-09-21 21:27:36 +0200

    wddde

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/Navigation/Navigation'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>OPLAAAAAAAAAA</h1>
+        <Navigation></Navigation>
+      </div>
+    </>
+  )
+}
+
+export default App
