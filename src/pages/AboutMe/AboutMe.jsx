1c96af9f3b78c139f1f3f31a7a546cab881378f3
illocarlos <carlosalbendiz93@gmail.com>
2024-01-21 18:34:24 +0100

    article about me

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -15,8 +15,7 @@ const AboutMe = () => {
                         <Col md={6} className="order-1 order-md-1">
 
                             <div className='articleAboutMe'>
-                                Soy un desarrollador web que cursé el bootcamp de Ironhack.
-                                Me destaco por mi capacidad para resolver problemas,
+                                Soy un desarrollador web que me destaco por mi capacidad para resolver problemas,
                                 trabajar bajo presión y ser responsable,
                                 habilidades que adquirí durante mis 7 años de servicio en el ejército.
                                 Desde que descubrí el mundo de la tecnología y la programación,
@@ -33,8 +32,8 @@ const AboutMe = () => {
                                 Desde que comencé mi formación,
                                 he tenido la oportunidad de trabajar en proyectos tanto individuales como en equipo.
                                 Esto me ha permitido adquirir experiencia en el desarrollo front-end,
-                                utilizando tecnologías como CSS, HTML y Bootstrap,React.js, así como en el desarrollo back-end,
-                                utilizando JavaScript, Node.js,Mongodb y Express.js. Además,
+                                utilizando tecnologías como CSS, HTML y Bootstrap,React,Vue,Tailwind y SASS así como en el desarrollo back-end,
+                                utilizando JavaScript,Firebase, Node.js,Mongodb y Express.js. Además,
                                 he trabajado en un entorno de trabajo basado en Scrum,
                                 lo que ha fortalecido aún más mis habilidades en el trabajo en equipo,
                                 la resolución de problemas y la gestión efectiva del tiempo
