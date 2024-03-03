b8860fa5dcedebfbbc3c4f76218ce48725fed029
illocarlos <carlosalbendiz93@gmail.com>
2024-03-03 20:45:28 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function App() {
       });
 
       handleLinkHoverEvents();
-    }, 100); // Aquí puedes ajustar el tiempo de espera según tus necesidades
+    }, 500); // Aquí puedes ajustar el tiempo de espera según tus necesidades
 
     return () => clearTimeout(timeout);
   }, []);
