9bffe212f3b90fc40bf13374b45adf2b88a10dec
illocarlos <carlosalbendiz93@gmail.com>
2024-03-05 15:16:04 +0100

    implent loader in work

--- src/components/WorkComponents/DetailsWork/DetailsWork.jsx
+++ src/components/WorkComponents/DetailsWork/DetailsWork.jsx
@@ -3,6 +3,7 @@ import './DetailsWork.css'
 import { Row, Col, Container } from "react-bootstrap"
 import { useEffect, useState } from 'react'
 import workService from '../../../service/work.service'
+import Loader from '../../Loader/Loader'
 const DetailsWork = () => {
 
 
@@ -52,7 +53,12 @@ const DetailsWork = () => {
                                     {work.icon && work.icon.map((icon, index) => (
                                         <Col key={index} sm={12} md={3} lg={4} className="iconOrder mt-lg-4">
 
-                                            <img src={`/assets/Icons/${icon}.svg`} />
+                                            {!work.icon ? (
+                                                <Loader />
+                                            ) : (
+
+                                                <img src={`/assets/Icons/${icon}.svg`} />
+                                            )}
 
                                         </Col>
                                     ))}
@@ -64,15 +70,18 @@ const DetailsWork = () => {
                         <div className='div-Link'>
                             <Link target='_blank' className="custom-button ml-2" to={work.link}>web</Link>
                             <Link target='_blank' className="custom-button ml-2" to={work.gitHub}>gitHub</Link>
-                            <Link target='_blank' className="custom-button ml-2" to={"/"}>volver</Link>
+                            <Link className="custom-button ml-2" to={"/"}>volver</Link>
                         </div>
                     </div>
 
                 </Col >
                 <Col sm={12} md={6} lg={6} className="order-sm-1 ">
                     <div>
-                        <img className='work-image' src={work.workImage} alt={`image of proyect${work.title}`} />
-
+                        {!work.icon ? (
+                            <Loader />
+                        ) : (
+                            <img className='work-image' src={work.workImage} alt={`image of proyect${work.title}`} />
+                        )}
                     </div>
                 </Col>
 
